22fa4656adfc3d9a70a7c5056192b928f24c28d4
hamza gacem <gacemh112@gmail.com>
2023-06-13 10:50:21 +0200

    get list

--- ApiGestionProduits/Models/Product.cs
+++ ApiGestionProduits/Models/Product.cs
@@ -15,5 +15,10 @@ namespace ApiGestionProduits.Models
         public string? Category { get; set; }
         public string? Image { get; set; }
         public int Rating { get; set; }
+
+        public string getList()
+        {
+            return "";
+        }
     }
 }
